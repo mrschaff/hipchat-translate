98cc1cd1422233b702f2d5aa423b8481ee22e292
mrschaff <pietroschaff@gmail.com>
2016-03-22 12:29:40 -0300

    file upload

---
+++ web.js
@@ -0,0 +1,60 @@
+var track = require('ac-koa-hipchat-keenio').track;
+
+var ack = require('ac-koa').require('hipchat');
+var pkg = require('./package.json');
+var app = ack(pkg);
+var get = require('simple-get');
+
+var yandex_token = 'trnsl.1.1.20160317T173735Z.2be744fb3540d280.25573e00053497a5547e1c8f01cce7376f48c9f9';
+var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='+yandex_token;
+
+var addon = app.addon()
+  .hipchat()
+  .allowGlobal(true)
+  .allowRoom(true)
+  .scopes('send_notification');
+
+track(addon);
+
+if (process.env.DEV_KEY) {
+  addon.key(process.env.DEV_KEY);
+}
+
+addon.webhook('room_message', /^\/translate(?:\s+(:)?(.+?)\s*$)?/i, function *() {
+    var global = !this.tenant.room;
+    var match = this.match;
+    var room = this.room;
+    var command = match && match[1] === ':' && match[2];
+    if (command) {
+    	if (/\w{2}-\w{2}\s.*/i.test(command)) {
+			var result = "";
+	    	var langtext = match[2].split(' ');
+			var urlcall = url+'&lang='+langtext[0]+'&text='+langtext.slice(1).join("+");
+			var me = this;
+			//console.log(urlcall);
+			get(urlcall, (err, res) => {
+				if (err) throw err
+				res.setEncoding('utf8');
+				res.on('data', (buffer) => {
+					if(buffer){
+						var resultObj = JSON.parse(buffer);
+						result += resultObj.text;
+					//	console.log(buffer);
+					}
+				});
+ 				res.on('end', () => {
+					me.roomClient.sendNotification(result);
+				});
+			});			
+		} else if (/\w{2}-\w{2}/i.test(command)) {
+			yield this.roomClient.sendNotification('Add a text to be translated!');
+		} else {
+			yield this.roomClient.sendNotification('Add a valid language to translate in the correct format (i.e: en-ru, fr-en)');
+		}	
+  	} else {
+		yield this.roomClient.sendNotification('<b>Usage</b>: translates the text you insert from language l1 to language l2 (i.e: /translate :en-fr home)');
+ 		yield this.roomClient.sendNotification('For the list of support languages please visit https://tech.yandex.com/translate/doc/dg/concepts/langs-docpage/');
+	}
+});
+
+app.listen();
