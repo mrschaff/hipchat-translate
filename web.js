396968f76d03be2e658a63d2431675f5e2b2e585
mrschaff <pietroschaff@gmail.com>
2016-03-23 18:31:20 -0300

    Update web.js

--- web.js
+++ web.js
@@ -36,7 +36,7 @@ addon.webhook('room_message', /^\/translate(?:\s+(:)?(.+?)\s*$)?/i, function *()
 					console.log(err);
 					me.roomClient.sendNotification('An error occurred. Please contact your administrator for more information.');
 				};
-				res.setEncoding('utf16');
+				//res.setEncoding('utf8');
 				res.on('data', (buffer) => {
 					if(buffer){
 						var resultObj = JSON.parse(buffer);
